02aa6eddab2d695dbca5a0bb62ff85986a346dd4
mkeloo <mkeloo@ufl.edu>
2023-01-07 18:43:13 -0500

    Footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,161 @@
 import React from 'react';
+import './Footer.css';
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const myAccount = [
+  {
+    display: 'Author Profile',
+    url: '/seller-profile',
+  },
+  {
+    display: 'Create Item',
+    url: '/create',
+  },
+  {
+    display: 'Collection',
+    url: '/market',
+  },
+  {
+    display: 'Edit Profile',
+    url: '/edit-profile',
+  },
+];
+
+const resources = [
+  {
+    display: 'Help Center',
+    url: '#',
+  },
+  {
+    display: 'Partner',
+    url: '#',
+  },
+  {
+    display: 'Community',
+    url: '#',
+  },
+  {
+    display: 'Activity',
+    url: '#',
+  },
+];
+
+const company = [
+  {
+    display: 'About',
+    url: '#',
+  },
+  {
+    display: 'Career',
+    url: '#',
+  },
+  {
+    display: 'Ranking',
+    url: '#',
+  },
+  {
+    display: 'Contact Us',
+    url: '/contact',
+  },
+];
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="6" sm="6" className="mb-4">
+            <div className="logo">
+              <h2 className=" d-flex gap-2 align-items-center ">
+                <span>
+                  <i class="ri-fire-fill"></i>
+                </span>
+                NFTs
+              </h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate, quod repellat! Quis quos dolorum tenetur fuga?
+                Aspernatur rerum quae amet.
+              </p>
+            </div>
+          </Col>
+          <Col lg="2" md="3" sm="6">
+            <h5>My Account</h5>
+            <ListGroup className="list__group">
+              {myAccount.map((link, index) => (
+                <ListGroupItem key={index} className="list__item">
+                  <Link to={link.url}>{link.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+
+          <Col lg="2" md="3" sm="6">
+            <h5>Resources</h5>
+            <ListGroup className="list__group">
+              {resources.map((link, index) => (
+                <ListGroupItem key={index} className="list__item">
+                  <Link to={link.url}>{link.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+
+          <Col lg="2" md="3" sm="6">
+            <h5>Company</h5>
+            <ListGroup className="list__group">
+              {company.map((link, index) => (
+                <ListGroupItem key={index} className="list__item">
+                  <Link to={link.url}>{link.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="6" sm="6">
+            <h5>Newsletter</h5>
+            <input type="text" className="newsletter" placeholder="Email" />
+            <div className="social__links d-flex gap-3 align-items-center">
+              <span>
+                <Link to="#">
+                  <i className="ri-facebook-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i className="ri-instagram-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i className="ri-twitter-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i className="ri-telegram-line"></i>
+                </Link>
+              </span>
+              <span>
+                <Link to="#">
+                  <i className="ri-discord-line"></i>
+                </Link>
+              </span>
+            </div>
+          </Col>
+
+          <Col lg="12" className="mt-4 text-center">
+            <p className="copyright">
+              {' '}
+              Copyrights {year}. Developed by Moksh Keloo. All Rights Reserved.{' '}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
